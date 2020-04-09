93b41fc2af17cf3daf86295c7d97ecdc54527dab
Hung Nguyen <hnx116@gmail.com>
2020-04-10 01:19:03 +0700

    add comment

--- kutex.go
+++ kutex.go
@@ -10,15 +10,18 @@ type Kutex struct {
 	size uint
 }
 
+// New creates a Kutex instance.
 func New(size uint) *Kutex {
 	return &Kutex{size: size, mus: make([]sync.Mutex, size, size)}
 }
 
+// Lock waits to acquire lock.
 func (me *Kutex) Lock(key string) {
 	i := uint(crc32.ChecksumIEEE([]byte(key)))
 	me.mus[i%me.size].Lock()
 }
 
+// Unlock release the lock.
 func (me *Kutex) Unlock(key string) {
 	i := uint(crc32.ChecksumIEEE([]byte(key)))
 	me.mus[i%me.size].Unlock()
